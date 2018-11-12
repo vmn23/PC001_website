ff317c809bb1662ce1b44a715760ac80694007a0
vmn23 <vmn23@cornell.edu>
2018-11-11 23:51:28 -0500

    Adding html files

--- oursite/landing/urls.py
+++ oursite/landing/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tutorial/', views.tutorial, name='tutorial'),
+    path('journey1/', views.journey1, name='journey1'),
+    path('journey2/', views.journey2, name='journey2'),
+    path('journey3/', views.journey3, name='journey3'),
+    path('journey4/', views.journey4, name='journey4'),
+    path('journey5/', views.journey5, name='journey5'),
+    path('login/', views.login, name='login'),
     path('homepage/', views.homepage, name='homepage'),
+    path('matches/', views.matches, name='matches'),
+    path('buildresume/', views.buildresume, name='buildresume'),
+    path('questionnaire/', views.questionnaire, name='questionnaire'),
 ]
--- oursite/landing/views.py
+++ oursite/landing/views.py
@@ -3,14 +3,45 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    #return HttpResponse("You are at the index.")
-    #template = loader.get_template('landing/index.html')
     context = {}
-    #return HttpResponse(template.render(context,request))
     return render(request, 'landing/index.html', context)
 
-def tutorial(request):
-    return HttpResponse("You are at the tutorial page.")
+def journey1(request):
+    context = {}
+    return render(request, 'landing/journey1.html', context)
+
+def journey2(request):
+    context = {}
+    return render(request, 'landing/journey2.html', context)
+
+def journey3(request):
+    context = {}
+    return render(request, 'landing/journey3.html', context)
+
+def journey4(request):
+    context = {}
+    return render(request, 'landing/journey4.html', context)
+
+def journey5(request):
+    context = {}
+    return render(request, 'landing/journey5.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'landing/login.html', context)
 
 def homepage(request):
-    return HttpResponse("You are at the homepage page.")
+    context = {}
+    return render(request, 'landing/homepage.html', context)
+
+def matches(request):
+    context = {}
+    return render(request, 'landing/matches.html', context)
+
+def buildresume(request):
+    context = {}
+    return render(request, 'landing/buildresume.html', context)
+
+def questionnaire(request):
+    context = {}
+    return render(request, 'landing/questionnaire.html', context)
